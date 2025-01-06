ae30cc38e9fb2291b2a072b67d85dedcec7fde16
Andrey Yavorsky <andrey@hut.dev>
2025-01-06 18:22:42 +0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -13,10 +13,18 @@ let package = Package(
             name: "NuveiSimplyConnectSDK",
             targets: ["NuveiSimplyConnectSDKWrapper"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/airsidemobile/JOSESwift", from: "2.4.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.8.2"),
+        .package(url: "https://github.com/roberthein/TinyConstraints", from: "4.0.2"),
+    ],
     targets: [
         .target(
             name: "NuveiSimplyConnectSDKWrapper",
             dependencies: [
+                .product(name: "JOSESwift", package: "JOSESwift"),
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+                .product(name: "TinyConstraints", package: "TinyConstraints"),
                 .target(name: "NuveiSimplyConnectSDKFramework")
             ],
             path: "SwiftPM/NuveiSimplyConnectSDKWrapper"
